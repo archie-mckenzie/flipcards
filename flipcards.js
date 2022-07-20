f3a69e4e02ed805e46ebc2e22f8577ff2d3ecab3
Archie McKenzie <archiem@princeton.edu>
2022-07-20 08:54:08 -0400

    added js part 1

---
+++ flipcards.js
@@ -0,0 +1,11 @@
+const deck = [
+    {
+        front: "images/front1.png",
+        back: "images/questions.png"
+    },
+    {
+        front: "images/front2.png",
+        back: "images/questions.png"
+    }
+];
+
