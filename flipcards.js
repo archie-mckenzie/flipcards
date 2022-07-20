68fc03b7d32166e88dc9b821baea1358b8842965
Archie McKenzie <archiem@princeton.edu>
2022-07-20 09:43:13 -0400

    shuffle test case

--- flipcards.js
+++ flipcards.js
@@ -1,16 +1,22 @@
+// array data structure which represents a deck of CEC People's Money cards
 const deck = [
     {
-        front: "images/front1.png",
+        front: "images/1front.png",
         back: "images/questions.png"
     },
     {
-        front: "images/front2.png",
+        front: "images/2front.png",
         back: "images/questions.png"
+    },
+    {
+        front: "images/3front.png",
+        back: "images/projects.png"
     }
 ];
 
-var position = 0;
+var position = 0; // starting position (first card in the deck)
 
+// moves left in the deck
 function goLeft() {
     if (position > 0) {
         var front = document.getElementById("front");
@@ -21,6 +27,7 @@ function goLeft() {
     }
 }
 
+// moves right in the deck
 function goRight() {
     if (position < deck.length -  1) {
         var front = document.getElementById("front");
@@ -52,5 +59,28 @@ function shuffle() {
     back.src = deck[position].back;
 }
 
+// Testing the shuffle function to see if it is truly random
+/* 
+function testShuffle() {
+    var one = 0;
+    var two = 0;
+    var three = 0;
 
-
+    const iterations = 10000;
+    
+    for (let i = 0; i < iterations; i++) {
+        var front = document.getElementById("front");
+        if (deck[0].front == "images/1front.png") {
+            one++;
+        } else if (deck[0].front == "images/2front.png") {
+            two++;
+        } else {
+            three++;
+        }
+        shuffle();
+    }
+    console.log("ONE: " + one);
+    console.log("TWO: " + two);
+    console.log("THREE: " + three);
+    console.log("TOTAL: " + iterations)
+} */
