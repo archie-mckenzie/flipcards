1d73065ccb797ebdee34d6f3569407a0bda6e84d
Archie McKenzie <archiem@princeton.edu>
2022-07-20 09:22:23 -0400

    added buttons

--- flipcards.js
+++ flipcards.js
@@ -9,3 +9,31 @@ const deck = [
     }
 ];
 
+var position = 0;
+
+function goLeft() {
+    if (position > 0) {
+        var front = document.getElementById("front");
+        var back = document.getElementById("back");
+        position--;
+        front.src = deck[position].front
+        back.src = deck[position].back
+    }
+}
+
+function goRight() {
+    if (position < deck.length -  1) {
+        var front = document.getElementById("front");
+        var back = document.getElementById("back");
+        position++;
+        front.src = deck[position].front
+        back.src = deck[position].back
+    }
+}
+
+function shuffle() {
+
+}
+
+
+
