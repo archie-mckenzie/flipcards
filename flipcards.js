f2d817483983d4a66145444b67ecd219b361c41b
Archie McKenzie <archiem@princeton.edu>
2022-07-20 09:29:14 -0400

    added shuffle

--- flipcards.js
+++ flipcards.js
@@ -16,8 +16,8 @@ function goLeft() {
         var front = document.getElementById("front");
         var back = document.getElementById("back");
         position--;
-        front.src = deck[position].front
-        back.src = deck[position].back
+        front.src = deck[position].front;
+        back.src = deck[position].back;
     }
 }
 
@@ -26,13 +26,30 @@ function goRight() {
         var front = document.getElementById("front");
         var back = document.getElementById("back");
         position++;
-        front.src = deck[position].front
-        back.src = deck[position].back
+        front.src = deck[position].front;
+        back.src = deck[position].back;
     }
 }
 
-function shuffle() {
+// swaps two cards in the deck, used for shuffle
+function swap(i, j) {
+    const temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+}
 
+// Fisher-Yates shuffle algorithm implementation
+function shuffle() {
+    for (let i = deck.length; i > 0; i--) {
+        const randInt = Math.floor(Math.random() * i);
+        const index = i - 1;
+        swap(index, randInt)
+    }
+    var front = document.getElementById("front");
+    var back = document.getElementById("back");
+    position = 0;
+    front.src = deck[position].front;
+    back.src = deck[position].back;
 }
 
 
